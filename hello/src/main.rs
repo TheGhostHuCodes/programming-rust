5698aea20e2bad0d448fdb4b36cd95ef35e54ec6
Yung-Jin (Joey) Hu <yungjinhu@gmail.com>
2021-06-09 10:34:21 -0700

    Update gcd program to have a tighter function signature

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,33 +1,26 @@
 use std::io::Write;
+use std::num::NonZeroU64;
 use std::str::FromStr;
 
-#[test]
-fn test_gcd() {
-    assert_eq!(gcd(2 * 5 * 11 * 17,
-                   3 * 7 * 13 * 19),
-               1);
-
-    assert_eq!(gcd(2 * 3 * 5 * 11 * 17,
-                   3 * 7 * 11 * 13 * 19),
-               3 * 11);
-}
-
-fn gcd(mut n: u64, mut m: u64) -> u64 {
-    assert!(n != 0 && m != 0);
-    while m != 0 {
-        if m < n {
-            let t = m; m = n; n = t
+fn gcd(n: NonZeroU64, m: NonZeroU64) -> NonZeroU64 {
+    let mut n_ = n.get();
+    let mut m_ = m.get();
+    while m_ != 0 {
+        if m_ < n_ {
+            let temp = m_;
+            m_ = n_;
+            n_ = temp
         }
-        m = m % n;
+        m_ = m_ % n_;
     }
-    n
+    NonZeroU64::new(n_).unwrap()
 }
 
 fn main() {
     let mut numbers = Vec::new();
 
     for arg in std::env::args().skip(1) {
-        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+        numbers.push(NonZeroU64::from_str(&arg).expect("error parsing argument"));
     }
 
     if numbers.len() == 0 {
@@ -36,9 +29,35 @@ fn main() {
     }
 
     let mut d = numbers[0];
-    for m in &numbers[1..] {
-        d = gcd(d, *m);
+    for &m in &numbers[1..] {
+        d = gcd(d, m);
     }
 
-    println!("The greatest common divisor if {:?} is {}",  numbers, d);
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::*;
+
+    #[test]
+    fn test_gcd() {
+        assert_eq!(
+            gcd(
+                NonZeroU64::new(2 * 5 * 11 * 17).unwrap(),
+                NonZeroU64::new(3 * 7 * 13 * 19).unwrap()
+            )
+            .get(),
+            1
+        );
+
+        assert_eq!(
+            gcd(
+                NonZeroU64::new(2 * 3 * 5 * 11 * 17).unwrap(),
+                NonZeroU64::new(3 * 7 * 11 * 13 * 19).unwrap()
+            )
+            .get(),
+            3 * 11
+        );
+    }
 }
